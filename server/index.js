f7390f217d5f7bfb9b93dd652ac2f4f46ae7f9fa
Shubham Das <subhamdas9800@gmail.com>
2024-09-03 12:57:30 +0530

    final deploy

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb", extented: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors({
-    origin: "http://localhost:3000", // The origin of your frontend
+    origin: "https://cloud-comm.vercel.app", // The origin of your frontend
     credentials: true,
   }));
 app.use("/assets",express.static(path.join(__dirname, "public/assets")));
