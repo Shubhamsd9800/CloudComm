04bdc1570a54afee14cff7bec39224d16b4b3d5f
Shubham Das <subhamdas9800@gmail.com>
2024-09-03 12:24:16 +0530

    deploy on way

--- server/index.js
+++ server/index.js
@@ -31,8 +31,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb", extented: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors({
-    origin: "http://cloudcomm.vercel.app", // The origin of your frontend
-    methods:["POST","GET","PATCH"],
+    origin: "http://localhost:3000", // The origin of your frontend
     credentials: true,
   }));
 app.use("/assets",express.static(path.join(__dirname, "public/assets")));
